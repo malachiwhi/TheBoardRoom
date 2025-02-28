1835d6ab9b4fba8dc12253b6b96bc951c6d4c2f2
Malachi <malachiwhite31@gmail.com>
2025-02-27 21:04:49 -0600

    Login and SignIn button as well as logo

---
+++ TheBoardRoom/SignUp.swift
@@ -0,0 +1,18 @@
+//
+//  SignUp.swift
+//  TheBoardRoom
+//
+//  Created by Malachi  on 2/27/25.
+//
+
+import SwiftUI
+
+struct SignUp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUp()
+}
