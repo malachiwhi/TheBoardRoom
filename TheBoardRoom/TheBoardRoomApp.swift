edd1223ac095f159a3cea87ed8df10cfe58d6be6
Malachi <malachiwhite31@gmail.com>
2025-02-27 20:39:29 -0600

    Initial Commit

---
+++ TheBoardRoom/TheBoardRoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheBoardRoomApp.swift
+//  TheBoardRoom
+//
+//  Created by Malachi  on 2/27/25.
+//
+
+import SwiftUI
+
+@main
+struct TheBoardRoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
