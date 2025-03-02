4e37e8d39de21265e1419f2fd9216f85cfd0eca3
Malachi <malachiwhite31@gmail.com>
2025-03-02 09:26:02 -0600

    View file and HomePage set up

---
+++ TheBoardRoom/View/HomePage.swift
@@ -0,0 +1,26 @@
+//
+//  HomePage.swift
+//  TheBoardRoom
+//  Homepage with bottom nav bar with a floating button as well as
+//  The boardRoom Video board and borad room writing section
+//  Also has a profile setting page as well along side it
+//  Left is the Writing section, middle is the video section, far right is the profile page section
+//  Created by Malachi  on 3/2/25.
+//
+
+import SwiftUI
+
+struct HomePage: View {
+    var body: some View {
+        ZStack {
+            VStack {
+                Text("Home Page")
+            }
+        }
+        
+    }
+}
+
+#Preview {
+    HomePage()
+}
